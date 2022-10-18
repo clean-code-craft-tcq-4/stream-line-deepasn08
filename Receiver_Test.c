3520c38aa65412d8fff49a9ae724864416a7e447
HriturajSR <105203856+HriturajSR@users.noreply.github.com>
2022-10-18 11:39:30 +0530

    Update Receiver_Test.c

--- Receiver_Test.c
+++ Receiver_Test.c
@@ -18,7 +18,7 @@ TEST_CASE("Tests to check whether sensor data is read from console and also to c
   //To check Max, Min and SMA values of Temperature
   expectedMaxValue = 45;
   expectedMinValue = 0;
-  expectedSMAValue = 21;
+  expectedSMAValue = 21.8;
   observedMaxValue = fetchMaxValue(&Temperature_value[0]);
   observedMinValue = fetchMinValue(&Temperature_value[0]);
   observedSMAValue = calculateMovingAverage(&Temperature_value[0]);
@@ -27,12 +27,12 @@ TEST_CASE("Tests to check whether sensor data is read from console and also to c
   REQUIRE(observedSMAValue == expectedSMAValue);
   
   //To check print to console function
-  REQUIRE(printReceivedDataToConsole(&Temperature_value[0],45,0,21) == 1);
+  REQUIRE(printReceivedDataToConsole(&Temperature_value[0],45,0,21.8) == 1);
           
   //To check Max, Min and SMA values of SOC
   expectedMaxValue = 80;
   expectedMinValue = 20;
-  expectedSMAValue = 58;
+  expectedSMAValue = 58.2;
   observedMaxValue = fetchMaxValue(&SOC_value[0]);
   observedMinValue = fetchMinValue(&SOC_value[0]);
   observedSMAValue = calculateMovingAverage(&SOC_value[0]);
@@ -41,5 +41,5 @@ TEST_CASE("Tests to check whether sensor data is read from console and also to c
   REQUIRE(observedSMAValue == expectedSMAValue);
   
   //To check print to console function
-  REQUIRE(printReceivedDataToConsole(&SOC_value[0],80,20,58) == 1);
+  REQUIRE(printReceivedDataToConsole(&SOC_value[0],80,20,58.2) == 1);
 }
